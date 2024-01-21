cc761019f332c2dda36c2a42c8fdfe573b4d4abf
Nurseit <nurseitbakytbekov@gmail.com>
2024-01-21 11:54:46 +0600

    init weather components

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  PropType,
+} from 'vue';
+import { Weather } from '../models/weather';
+
+export default defineComponent({
+  name: 'WeatherCard',
+  props: {
+    title: {
+      type: String as PropType<Weather['title']>,
+      required: true
+    }
+  },
+  setup (props) {
+    return {};
+  },
+});
+</script>
---
+++ src/components/WeatherContent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="product-list">
+      <WeatherCard title="Weather card"/>
+    </div>
+  </template>
+  
+  <script>
+  import WeatherCard from './WeatherCard.vue';
+  
+  export default {
+    name: 'WeatherContent',
+    components: { WeatherCard },
+  };
+  </script>
