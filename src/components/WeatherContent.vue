d33d88dcb86b7ac146e81a60c60fa5b34d95e5b2
Nurseit <nurseitbakytbekov@gmail.com>
2024-01-21 15:23:39 +0600

    create select for choose City

--- src/components/WeatherContent.vue
+++ src/components/WeatherContent.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="product-list">
-      <WeatherCard title="Weather card"/>
-    </div>
-  </template>
-  
-  <script>
-  import WeatherCard from './WeatherCard.vue';
-  
-  export default {
-    name: 'WeatherContent',
-    components: { WeatherCard },
-  };
-  </script>
+  <div class="product-list">
+    <WeatherCitySelect />
+    <WeatherCard title="Weather card" />
+  </div>
+</template>
+
+<script>
+import WeatherCard from './WeatherCard.vue';
+import WeatherCitySelect from './WeatherCitySelect.vue';
+
+export default {
+  name: 'WeatherContent',
+  components: { WeatherCard, WeatherCitySelect },
+};
+</script>
