3fb9c54a21fabc83182ca877677ed43e846aa6bf
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-01-22 18:54:54 +0600

    add translations

--- src/i18n/ru-RU/index.ts
+++ src/i18n/ru-RU/index.ts
@@ -1,5 +1,11 @@
 export default {
-    failed: 'Ошибка',
-    success: 'Успех'
+    'failed': 'Ошибка',
+    'success': 'Успех',
+    'Weather App': 'Приложение погоды',
+    'or': 'или',
+    'Find out the weather in your city': 'Узнайте погоду в вашем городе',
+    'Specify my location': 'Указать мое местоположение',
+    'Select city': 'Выберите город',
+    'Change language': 'Сменить язык',
   };
   
