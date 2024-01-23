8bb5d606b735ded49c3f254e3367fa9a7e6fd4f8
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-01-23 11:28:12 +0600

    fix problems with theme switch

--- src/components/WeatherByLocation.vue
+++ src/components/WeatherByLocation.vue
@@ -1,13 +1,25 @@
 <template>
-        <div class="col text-black text-center">
-      <div class="text-h5 text-weight-light">{{ $t('Find out the weather in your city') }}</div>
-      <q-separator size="1px" color="black" spaced />
-      <div class="text-h6 text-weight-light">
-        <q-btn @click="getUserCoordinates" color="primary">
-          <q-icon name="location_on" />{{$t('Specify my location')}}
-        </q-btn>
-      </div>
+  <div class="col text-black text-center">
+    <div
+      class="text-h5 text-weight-light"
+      :class="{
+        'text-dark': !$q.dark.isActive,
+        'text-white': $q.dark.isActive,
+      }"
+    >
+      {{ $t('Find out the weather in your city') }}
     </div>
+    <q-separator
+      size="1px"
+      :color="$q.dark.isActive ? 'white' : 'black'"
+      spaced
+    />
+    <div class="text-h6 text-weight-light">
+      <q-btn @click="getUserCoordinates" color="primary">
+        <q-icon name="location_on" />{{ $t('Specify my location') }}
+      </q-btn>
+    </div>
+  </div>
 </template>
 
 <script>
