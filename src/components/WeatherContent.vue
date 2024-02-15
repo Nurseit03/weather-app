07e15a6f32801a30a5949b4417876ae973306212
Nurseit <nurseitbakytbekov@gmail.com>
2024-02-15 16:09:31 +0600

    fix: get weather by user coordinates

--- src/components/WeatherContent.vue
+++ src/components/WeatherContent.vue
@@ -4,7 +4,11 @@
       :options="citiesList.cities"
       @onSelect="handleSelectCity"
     />
-    <WeatherCard v-if="weatherData.main" :weatherData="weatherData" :cityData="cityData" />
+    <WeatherCard
+      v-if="weatherData.main"
+      :weatherData="weatherData"
+      :cityData="cityData"
+    />
     <div class="text-h6 text-weight-light">{{ $t('or') }}</div>
     <WeatherByLocation :getUserCoordinates="getUserCoordinates" />
   </div>
@@ -27,13 +31,18 @@ let cityData = reactive<ICity>({
   label: null,
   lat: null,
   lng: null,
-  country: null
+  country: null,
 });
 
 let userCoordinates = reactive({
   latitude: null as number | null,
   longitude: null as number | null,
 });
+
+const handleSelectCity = (selectedCity: ICity) => {
+  cityData = selectedCity;
+  getWeatherByCoordinates(selectedCity.lat, selectedCity.lng);
+};
 
 const getUserCoordinates = () => {
   navigator.geolocation.getCurrentPosition((position) => {
@@ -59,9 +68,18 @@ const getWeatherByCoordinates = (
     });
 };
 
-const handleSelectCity = (selectedCity: ICity) => {
-  cityData = selectedCity;
-  getWeatherByCoordinates(selectedCity.lat, selectedCity.lng);
+const compareUserCoordinates = (newLatitude: number, newLongitude: number) => {
+  const { latitude, longitude } = userCoordinates;
+
+  if (latitude !== newLatitude || longitude !== newLongitude) {
+    resetObject(cityData);
+  }
+};
+
+const resetObject = (obj: any) => {
+  Object.keys(obj).forEach((key) => {
+    obj[key as keyof typeof obj] = null;
+  });
 };
 
 watch(userCoordinates, () => {
@@ -70,6 +88,7 @@ watch(userCoordinates, () => {
       userCoordinates?.latitude,
       userCoordinates?.longitude
     );
+    compareUserCoordinates(cityData.lat ?? 0, cityData.lng ?? 0);
   }
 });
 </script>
