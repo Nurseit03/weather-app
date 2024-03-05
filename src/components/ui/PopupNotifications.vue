acd598cb6e099035b2bc9cda8fe9e0f9165dc292
Nurseit <nurseitbakytbekov@gmail.com>
2024-03-06 01:15:01 +0600

    add notifications

--- src/components/ui/PopupNotifications.vue
+++ src/components/ui/PopupNotifications.vue
@@ -1,25 +1,31 @@
 <template>
-  <q-btn round icon="notifications" @click="showNotifications">
+  <q-btn round icon="notifications">
     <q-badge v-if="showBadge" :class="'fade-in'" floating color="red" rounded>
       {{ notifications.length }}
     </q-badge>
 
     <q-menu anchor="top right" self="bottom right">
-      <q-list v-if="notifications?.length > 0" link>
+      <q-list v-if="notifications?.length > 0" link class="notifications-list">
         <q-item
           v-for="notification in displayedNotifications"
           :key="notification.id"
         >
-          <q-item-section>{{ notification?.message }}</q-item-section>
-          <q-item-section side top>{{ notification?.date }}</q-item-section>
+          <div class="q-item__section text-caption text-left text-bold">
+            <q-item-section>{{ notification?.message }}</q-item-section>
+            <q-item-section left class="notification-date">{{
+              formatDate(notification?.date)
+            }}</q-item-section>
+          </div>
         </q-item>
-        <q-infinite-scroll :handler="loadMoreNotifications" :offset="offset">
-          <template v-slot:loading> Загрузка... </template>
-        </q-infinite-scroll>
       </q-list>
       <q-item v-else>
-        <q-item-section class="text-caption text-center text-bold">
-          Нет уведомлений
+        <q-item-section class="caption text-center text-bold">
+          {{ $t('No notifications') }}
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section class="text-center">
+          <q-btn :label="$t('Close')" v-close-popup />
         </q-item-section>
       </q-item>
     </q-menu>
@@ -29,40 +35,48 @@
 <script lang="ts">
 import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
+import { format } from 'date-fns';
 
 export default {
   setup() {
     const $store = useStore();
-    const notifications = computed(
-      () => $store.getters['notifications/allNotifications']
-    );
+    const notifications = computed(() => $store.getters.allNotifications);
     const showBadge = computed(
       () => !!notifications.value && notifications.value.length > 0
     );
 
-    const showMenu = ref(false);
-    const offset = ref(5); 
+    function formatDate(date: Date): string {
+      return format(date, 'dd.MM.yyyy hh:mm');
+    }
+
+    const offset = ref(5);
     const displayedNotifications = computed(() =>
       notifications.value.slice(0, offset.value)
     );
 
-    const showNotifications = () => {
-      showMenu.value = !showMenu.value;
-    };
-
     const loadMoreNotifications = () => {
-      offset.value += 5; 
+      offset.value += 5;
     };
 
     return {
-      showBadge,
-      notifications,
-      showNotifications,
-      showMenu,
       offset,
-      displayedNotifications,
+      showBadge,
+      formatDate,
+      notifications,
       loadMoreNotifications,
+      displayedNotifications,
     };
   },
 };
 </script>
+
+<style scoped>
+.notifications-list {
+  max-width: 240px !important;
+  max-height: 280px !important;
+}
+.notification-date {
+  margin: 0;
+  color: rgba(0, 0, 0, 0.6);
+}
+</style>
