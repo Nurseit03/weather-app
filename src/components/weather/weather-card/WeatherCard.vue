f3fedb3fb587e4a50cbc7045ac87f9fbc99ef5e0
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-03-12 12:14:08 +0600

    translucent background

--- src/components/weather/weather-card/WeatherCard.vue
+++ src/components/weather/weather-card/WeatherCard.vue
@@ -87,7 +87,7 @@ export default {
 <style>
 .my-card {
   width: 90vw;
-  background-color: inherit;
+  background-color: rgba(255, 255, 255, 0.1);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
   @media (min-width: 768px) {
     width: 420px;
